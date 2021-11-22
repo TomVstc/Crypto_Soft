1b320857b3cb49528650d50f7e1c236af1b26e86
TomVi <tomv59@outlook.fr>
2021-11-22 15:33:03 +0100

    Ajout Sauvegarde fichier / répertoire - Gestion des erreurs (si path pas bon, name pas bon, ...) - Ajout automatique de l'extension sur un save fichier

--- Livrable/Model.cs
+++ Livrable/Model.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Livrable
 {
@@ -23,18 +24,43 @@ namespace Livrable
 
         public void createSave()
         {
-            if(Save.Type == "Complet")
+            if(Save.Type == "Full")
             {
-                string fileName = save.Name;
-                string fileSource = save.FileSource;
-                string fileTarget = save.FileTarget;
+                if(Save.Destination == "File")
+                {
+                    string fileName = save.Name + "." + save.Extension;
+                    string fileSource = save.FileSource;
+                    string fileTarget = save.FileTarget;
+                    string path = fileTarget + @"\" + fileName;
 
-                System.IO.File.Copy(fileSource, fileTarget + @"\" + fileName, true);
+                    System.IO.File.Copy(fileSource, path, true);
 
+                    Console.WriteLine("File Create\n");
+                }
+                if(Save.Destination == "Directory")
+                {
+                    string fileName = save.Name;
+                    string fileSource = save.FileSource;
+                    string fileTarget = save.FileTarget + @"\" + save.Name;
+
+                    string[] files = System.IO.Directory.GetFiles(fileSource);
+                    string destFile = System.IO.Path.Combine(fileTarget, fileName);
+
+                    // Copy the files and overwrite destination files if they already exist.
+                    foreach (string s in files)
+                    {
+                        // Use static Path methods to extract only the file name from the path.
+                        fileName = System.IO.Path.GetFileName(s);
+                        destFile = System.IO.Path.Combine(fileTarget, fileName);
+                        System.IO.File.Copy(s, destFile, true);
+                    }
+
+                    Console.WriteLine("\nDirectory Create Create\n");
+                }
             }
-            if (Save.Type == "Differentiel")
+            if (Save.Type == "Differential")
             {
-                Console.WriteLine("Coucou c'est differentiel");
+                Console.WriteLine("\nDifferential no increment");
             }
 
         }
--- Livrable/ViewSave.cs
+++ Livrable/ViewSave.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Linq;
 
 namespace Livrable
 {
@@ -12,6 +13,8 @@ namespace Livrable
         private string fileSource;
         private string fileTarget;
         private string type;
+        private string destination;
+        private string extention;
 
         private IController controller;
 
@@ -41,6 +44,17 @@ namespace Livrable
             get { return Controller; }
             set { Controller = value; }
         }
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+        public string Extension
+        {
+            get { return extention; }
+            set { extention = value; }
+        }
+
 
         // Constructor call at the creation
         public ViewSave()
@@ -62,66 +76,173 @@ namespace Livrable
         {
             bool isSaveValid = false;
 
-            Console.WriteLine("\nEntrez les informations concernant la sauvegarde : ");
+            Console.WriteLine("\nEnter save information : \n");
 
-            while(isSaveValid != true)
+            getDestination();
+
+            if(destination == "File") // COPY OF A FILE
             {
-                Console.WriteLine("Veuillez entrer le nom de votre sauvegarde");
-                Name = Console.ReadLine();
-                Console.WriteLine("Veuillez entrer le fichier source (chemin)");
-                FileSource = Console.ReadLine();
-                Console.WriteLine("Veuillez entrer le chemin de destination");
-                FileTarget = Console.ReadLine();
-                Console.WriteLine("Veuillez entrer le type de sauvegarde : Complet / Differentiel");
-                Type = Console.ReadLine();
+                while (isSaveValid != true)
+                {
+                    getName();
+                    getSourcePathFile();
+                    getDestinationPathDirectory();
+                    getType();
+                    Extension = FileSource.Split(".").Last();
 
-                isSaveValid = checkIfSaveInputIsValid();
+                    isSaveValid = true;
+                }
+                controller.updateSave();
             }
+            else // COPY OF A DIRECTORY
+            {
+                while (isSaveValid != true)
+                {
+                    getName();
+                    
+                    getSourcePathDirectory();
+                    getDestinationPathDirectory();
+                    getType();
 
-            controller.updateSave();
-
+                    isSaveValid = true;
+                }
+                controller.updateSave();
+            }
         }
 
-        // Function to valid if a save is in good type
-        public bool checkIfSaveInputIsValid()
+        // RECUP AND CHECK THE SOURCE DIRECTORY
+        public void getSourcePathDirectory()
         {
-            bool isValid = false;
-            if(Name != "")
+            bool isSourcePathValid = false;
+            while (isSourcePathValid != true)
             {
-                if (File.Exists(FileSource))
-                {
-                    if (Directory.Exists(FileTarget))
-                    {
-                        if (Type == "Complet" || Type == "Differentiel")
-                        {
-                            isValid = true;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Type invalide");
-                            startSave();
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Destination invalide");
-                        startSave();
-                    }
+                Console.WriteLine("Enter the directory that you want to copy :");
+                fileSource = Console.ReadLine();
+                isSourcePathValid = checkSourcePathDirectory(fileSource);
+            }
+        }
+        public bool checkSourcePathDirectory(string SourceDirectory)
+        {
+            bool result = Directory.Exists(SourceDirectory);
+            return result;
+        }
 
-                }
-                else
-                {
-                    Console.WriteLine("Source invalide");
-                    startSave();
-                }
+        // RECUP AND CHECK THE DESTINATION
+        public void getDestination()
+        {
+            bool isDestinationValid = false;
+            while (isDestinationValid != true)
+            {
+                Console.WriteLine("Copy File : File // Copy Directory : Directory");
+                destination = Console.ReadLine();
+                isDestinationValid = checkDestination(destination);
+            }
+        }
+        public bool checkDestination(string type)
+        {
+            if (type == "File" || type == "Directory")
+            {
+                return true;
             }
             else
             {
-                Console.WriteLine("Nom invalide");
-                startSave();
+                return false;
             }
+        }
 
-            return isValid;
+        // RECUP AND CHECK FOR A FILE
+        public void getSourcePathFile()
+        {
+            bool isSourcePathValid = false;
+            while (isSourcePathValid != true)
+            {
+                Console.WriteLine("Enter the source path where your file is : ");
+                FileSource = Console.ReadLine();
+                isSourcePathValid = checkSourcePathFile(FileSource);
+            }
+        }
+        public bool checkSourcePathFile(string FileSource)
+        {
+            bool result = File.Exists(FileSource);
+            return result;
+        }
+
+        // RECUP AND CHECK FOR A DIRECTORY
+        public void getDestinationPathDirectory()
+        {
+            bool isSourcePathValid = false;
+            while (isSourcePathValid != true)
+            {
+                Console.WriteLine("Enter the destination path where your will put the file : ");
+                FileTarget = Console.ReadLine();
+                isSourcePathValid = checkDestinationPathDirectory(FileTarget);
+            }
+        }
+        public bool checkDestinationPathDirectory(string FileTarget)
+        {
+
+            if (destination == "Directory")
+            {
+                string NameFile = FileTarget + @"\" + name;
+                bool result = Directory.Exists(NameFile);
+                if (result == false)
+                {
+                    FileTarget = NameFile;
+                    System.IO.Directory.CreateDirectory(FileTarget);
+                    result = true;
+                }
+                return result;
+            }
+            else
+            {
+                bool result = Directory.Exists(FileTarget);
+                return result;
+            }
+            
+            
+        }
+
+        // RECUP AND CHECK THE NAME
+        public void getName()
+        {
+            bool isNameValid = false;
+            while (isNameValid != true)
+            {
+                Console.WriteLine("Enter the name of your save : ");
+                Name = Console.ReadLine();
+                isNameValid = checkName(Name);
+            }
+        }
+        public bool checkName(string Name)
+        {
+            if(Name == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // RECUP AND CHECK THE TYPE
+        public void getType()
+        {
+            bool isTypeValid = false;
+            while (isTypeValid != true)
+            {
+                Console.WriteLine("Enter the type (Full / Differential)");
+                type = Console.ReadLine();
+                isTypeValid = checkType(type);
+            }
+        }
+        public bool checkType(string type)
+        {
+            if (type == "Full" || type == "Differential")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
